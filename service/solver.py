d0e667846e2cd772570f2651c1aa63e38841099f
YSRKEN <ysr.ken@gmail.com>
2020-10-13 20:15:47 +0900

    [clean] ソルバー関係のコードを別ファイルに移した

--- service/solver.py
+++ service/solver.py
@@ -1,8 +1,10 @@
+import json
 from typing import List
 
 from model.CellPos import CellPos
 from model.CellType import CellType
 from model.SunGlass import SunGlass
+from service.utility import show_board_data
 
 
 def create_board_from_problem(problem: SunGlass) -> List[CellType]:
@@ -29,3 +31,39 @@ def create_board_from_problem(problem: SunGlass) -> List[CellType]:
         for i in range(1, len(bridge.cell) - 1):
             board[get_pos(bridge.cell[i])] = CellType.BLANK
     return board
+
+
+def solve(problem: SunGlass) -> None:
+    """問題データから計算を行い、解答を標準出力で返す
+
+    Parameters
+    ----------
+    problem
+        問題データ
+
+    Returns
+    -------
+        標準出力で処理結果を返す
+    """
+
+    # 初期盤面データを作成する
+    board = create_board_from_problem(problem)
+    show_board_data(board, problem)
+
+
+def solve_from_path(path: str) -> None:
+    """問題ファイルをファイルパスから読み込んで処理する
+
+    Parameters
+    ----------
+    path
+        ファイルパス
+
+    Returns
+    -------
+        標準出力で処理結果を返す
+    """
+    with open(path) as f:
+        problem = SunGlass.from_dict(json.load(f))
+
+    solve(problem)
