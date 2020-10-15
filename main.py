d2009acf660c717f505f1b1cbd7c430d9b4cdf4e
YSRKEN <ysr.ken@gmail.com>
2020-10-15 11:33:59 +0900

    [add] README.mdを修正

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from service.solver import solve_from_path
 
 if __name__ == '__main__':
-    # solve_from_path('sample_problem.json')
-    # solve_from_path('problem/nyoroppyi-1206580414657089537.json')
-    # solve_from_path('problem/fuwa_lica_chan-1314913582467883009.json')
-    solve_from_path('problem/fuwa_lica_chan-1180103954417373185.json')
+    solve_from_path('sample_problem.json')
